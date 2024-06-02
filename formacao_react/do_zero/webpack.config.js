7dd3775fd6f8fce09d075e17b71613c118cada83
Mich <125368412+michellycruz@users.noreply.github.com>
2024-06-02 01:59:17 -0300

    estrutura do zero

--- formacao_react/do_zero/webpack.config.js
+++ formacao_react/do_zero/webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
     resolve: {
         extensions: [".js", ".jsx"],
     },
-    plugin: [
+    plugins: [
         new HtmlWebpackPlugin ({
             template: './public/index.html'
         })
