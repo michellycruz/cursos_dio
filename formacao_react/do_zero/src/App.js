0391a8c9cc71454f349883fd11eb925f60c2e65b
Mich <michxcrz@gmail.com>
2024-06-07 13:16:51 -0300

    Update App.js

--- formacao_react/do_zero/src/App.js
+++ formacao_react/do_zero/src/App.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
   const [usuarios, setUsuarios] = useState (
     ["Pablo", "José", "Manoel"]
-  )
+)
+
   return (
     <div className="App">
       <h1>Hello world</h1>
