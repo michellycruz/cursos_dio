5ec23baa77904038a0844f697dd14426485f11bd
Mich <125368412+michellycruz@users.noreply.github.com>
2024-06-14 22:51:21 -0300

    merda

--- formacao_react/primeira_aplicacao_em_react/gitfind/src/pages/Home/index.js
+++ formacao_react/primeira_aplicacao_em_react/gitfind/src/pages/Home/index.js
@@ -1,9 +1,14 @@
 import {Header} from '../../components/Header'
+import background from '../../assets/git.png'
+import './styles.css'
 
 function App() {
   return (
     <div className="App">
       <Header />  
+      <div className='conteudo'>
+        <img src={background} className='background' alt='background app' />
+      </div>
     </div>
   );
 }
